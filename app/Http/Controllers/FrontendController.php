279668c912182200e053b9c0347394eff5fa3196
Rashel124 <webcoderitrashel124@gmail.com>
2025-05-15 20:39:44 -0700

    update

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontendController extends Controller
+{
+    public function index(){
+        return view('frontend.index');
+    }
+
+    // public function contactUs(){
+    //     return view('frontend.contact');
+    // }
+
+
+  public function shopProducts(){
+        return view('frontend.shop');
+    }
+
+
+
+    public function returnProcess(){
+        return view('frontend.return-process');
+    }
+
+
+    public function productDetails(){
+        return view('frontend.product-details');
+    }
+
+    public function typeProducts(){
+        return view('frontend.type-products');
+    }
+}
+
+
+  
+
+
+
+
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\FrontendController;
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\Routing\RouteCompilerInterface;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// function testName (){
+//     return view('Welcome')
+// }
+
+
+
+Route::get('/',[FrontendController::class, 'index']);
+// Route::get('/contact-us', [FrontendController::class, 'contactUs']);
+Route::get('/shop',[FrontendController::class, 'shopProducts']);
+Route::get('/return-process',[FrontendController::class, 'returnProcess']); 
+Route::get('/product-details',[FrontendController::class, 'productDetails']); 
+Route::get('/type-products',[FrontendController::class, 'typeProducts']); 
